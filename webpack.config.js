4694641999b1afd4cea0ea1cb949dd15034bb39a
Colin Greenhill <clgreenhill@gmail.com>
2017-04-19 10:58:20 -0400

    Project setup changes - added scripts for auto build

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,18 @@
+var webpack = require("webpack");
+
 module.exports = {
     entry: "./src/app.tsx",
     output: {
         filename: "bundle.js",
-        path: __dirname + "/dist"
+        path: __dirname + "/dist/assets/js"
     },
 
     // Enable sourcemaps for debugging webpack's output.
-    devtool: "source-map",
+    //devtool: "source-map",
 
     resolve: {
         // Add '.ts' and '.tsx' as resolvable extensions.
-        extensions: [".ts", ".tsx", ".js", ".json"]
+        extensions: [".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
     },
 
     module: {
@@ -23,12 +25,5 @@ module.exports = {
         ]
     },
 
-    // When importing a module whose path matches one of the following, just
-    // assume a corresponding global variable exists and use that instead.
-    // This is important because it allows us to avoid bundling all of our
-    // dependencies, which allows browsers to cache those libraries between builds.
-    externals: {
-        "react": "React",
-        "react-dom": "ReactDOM"
-    },
+    plugins: [new webpack.optimize.CommonsChunkPlugin({ name: "globals", filename: "globals.js" })]
 };
