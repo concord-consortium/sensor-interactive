d6312b5b34d3dceb8b6455de8cb7af6688d34b59
Colin Greenhill <clgreenhill@gmail.com>
2017-04-19 13:27:33 -0400

    Added graph display [#143671555]

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { Title } from "./title";
+import { Graph } from "./graph";
 import SensorConnectorInterface from "@concord-consortium/sensor-connector-interface";
 
 export interface AppProps {};
@@ -12,18 +13,21 @@ const SENSOR_IP = "http://127.0.0.1:11180";
 
 export interface AppState {
     sensorActive:boolean,
-    sensorValue:number|undefined
+    sensorValue:number|undefined,
+    sensorData:number[][]
 }
 
 export class App extends React.Component<AppProps, AppState> {
     
     private sensor:SensorConnectorInterface;
+    private sensorColumnLengths:number[] = [];
     
     constructor(props: AppProps) {
         super(props);
         this.state = {
             sensorActive:false,
-            sensorValue:undefined
+            sensorValue:undefined,
+            sensorData:[[0,0]]
         };
         this.sensor = new SensorConnectorInterface();
         this.sensor.on("*", (e)=> {
@@ -31,6 +35,7 @@ export class App extends React.Component<AppProps, AppState> {
                 sensorActive:this.sensorHasData(),
                 sensorValue:this.getSensorValue()
             });
+            this.onSensorData(e);
         });
         this.sensor.startPolling(SENSOR_IP);
     }
@@ -47,6 +52,45 @@ export class App extends React.Component<AppProps, AppState> {
         return val;
     }
     
+    startSensor() {
+        this.sensor.requestStart();
+    }
+    
+    stopSensor() {
+        this.sensor.requestStop();
+    }
+    
+    onSensorData(setId:string) {
+        this.sensor.datasets.forEach(function(dataset) {
+
+            dataset.columns.forEach(function(column, columnIndex) {
+                
+                var lastLength = this.sensorColumnLengths[column.id];
+                if (lastLength === undefined) {
+                    lastLength = this.sensorColumnLengths[column.id] = 0;
+                }                    
+
+                // check there's new data for this column
+                if (column.data.length > lastLength) {
+                    if(columnIndex == 0) return; //time data
+                    
+                    var newData = column.data.slice(lastLength);
+
+                    // add new data to the graph
+                    var updatedData = this.state.sensorData;
+                    newData.forEach(function(data, rowIndex) {
+                        updatedData.push([updatedData.length, data]);
+                    });
+                    this.setState({
+                        sensorData: updatedData
+                    });
+                    this.render();
+                    this.sensorColumnLengths[column.id] = column.data.length;
+                }
+            }, this);
+        }, this);
+    }
+    
     renderSensorValue() {
         return (
             <div>
@@ -55,12 +99,23 @@ export class App extends React.Component<AppProps, AppState> {
             </div>
         );
     }
+    
+    renderGraph() {
+        return <Graph data={this.state.sensorData}/>
+    }
 
     render() {
         return (
             <div>
-                <Title />
-                {this.renderSensorValue()}
+                <div>
+                    <Title />
+                    {this.renderSensorValue()}
+                </div>
+                {this.renderGraph()}
+                <div>
+                    <button id="startSensor" onClick={()=>{this.startSensor()}}>Start</button>
+                    <button id="stopSensor" onClick={()=>{this.stopSensor()}}>Stop</button>
+                </div>
             </div>
         );
     }
