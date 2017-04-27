1f279b812a02acc779b05cc6aac9efc68adc0b9c
Colin Greenhill <clgreenhill@gmail.com>
2017-04-27 15:39:49 -0400

    Added deploy script for gh-pages

---
+++ deploy.sh
@@ -0,0 +1,54 @@
+DISTDIR="./dist"
+GITCONFIG="$DISTDIR/.git/config"
+HERE=`pwd`
+DATE=`date +"%Y-%m-%d"`
+SHA=`git rev-parse --short=8 HEAD`
+
+echo "Building gh-pages branch in $DISTDIR for SHA $SHA on $DATE"
+
+# 1) make sure dist folder variable is not empty and folder exists
+if [ -z "$DISTDIR" ]; then
+  echo "DISTDIR variable is empty, aborting!"
+  exit 1
+fi
+mkdir -p $DISTDIR
+
+# 2) Checkout a new clone of our repo into the dist directory
+# which will contain the gh-pages branch we will deploy
+if [ -e "$GITCONFIG" ]
+then
+  echo "found existing git clone in $DISTDIR"
+  cd $DISTDIR &&\
+  git checkout gh-pages &&\
+  git pull
+else
+  echo "cloning building-models repo in $DISTDIR"
+  rm -rf "$DISTDIR/*" &&\
+  git clone git@github.com:concord-consortium/sensor-interactive.git $DISTDIR &&\
+  cd $DISTDIR &&\
+  echo "checking out gh-pages branch" &&\
+  git checkout gh-pages
+fi
+
+# 3) clear out the dist directory
+echo "Clearing out existing files"
+git rm -rf .
+git clean -fxd
+
+cd $HERE
+
+# 4) Build our project using gulp into the dist folder
+echo "Rebuilding app"
+BUILD_INFO="$SHA built on $DATE"
+npm run build
+
+
+# 5) Deploy to gh-pages
+echo
+echo Deploying to gh-page
+echo
+cd $DISTDIR
+git add * &&\
+git commit -a -m "deployment for $SHA built on $DATE" &&\
+git push origin gh-pages
+
