c1a807f708c69b35aa7defa0ec81ce3ef7e8c95e
mklewandowski <matt.kain.lewandowski@gmail.com>
2019-03-19 09:48:40 -0700

    Have default initial page load the wired/wireless version of sensor-interactive

--- src/app.tsx
+++ src/app.tsx
@@ -2,15 +2,11 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import * as ReactModal from "react-modal";
 import { App } from "./components/app";
-import { SensorConnectorManager } from "./models/sensor-connector-manager";
-
-let sensorManager = new SensorConnectorManager();
 
 const appElt = document.getElementById("app");
 ReactModal.setAppElement(appElt);
 
 ReactDOM.render(
-    <App
-      sensorManager={sensorManager}/>,
-    appElt
+  <App/>,
+  appElt
 );
