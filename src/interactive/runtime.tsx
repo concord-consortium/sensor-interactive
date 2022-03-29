d6777df3479fa76c047fc6174b81f888f558ae05
Noah Paessel <knowuh@gmail.com>
2022-03-29 10:49:33 -0400

    181118888 Use authored state
    - Use authored state in enabling prediction.
    
    - reduce font size for buttons
    
    - TODO:  Need to be more performant with graph drawing (data-set merges & updates)
    
    [#181118888]
    https://www.pivotaltracker.com/story/show/181118888

--- src/interactive/runtime.tsx
+++ src/interactive/runtime.tsx
@@ -14,7 +14,7 @@ export const RuntimeComponent: React.FC<Props> = ({initMessage}) => {
   // NOTE: we only use the interactive state from startup, the sensor app maintains its own state during runtime.
   // The interactive state is saved to display at runtime startup and the report and report-item interactive views.
   const initialInteractiveState = initMessage.interactiveState;
-  const {prompt, hint, recordedData} = authoredState;
+  const {prompt, hint, recordedData, usePrediction} = authoredState;
   const { setInteractiveState } = useInteractiveState<IInteractiveState>();
   if(hint) { setHint(hint) };
   return (
@@ -27,6 +27,7 @@ export const RuntimeComponent: React.FC<Props> = ({initMessage}) => {
         maxGraphHeight={625}
         initialInteractiveState={initialInteractiveState}
         preRecordings ={recordedData ? [recordedData] : []}
+        requirePrediction={usePrediction}
         setInteractiveState={setInteractiveState}
       />
   );
